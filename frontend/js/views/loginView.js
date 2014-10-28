f9b8aa2983bac57c631472eb3c7ba84b86cbba16
William Blankenship <william.jblankenship@gmail.com>
2014-10-27 19:51:10 -0500

    Added bypass fuction

--- frontend/js/views/loginView.js
+++ frontend/js/views/loginView.js
@@ -1,17 +1,24 @@
 var LoginView = Backbone.View.extend({
   defaultLocation: "body",
-	initialize: function(){
+  initialize: function(){
     this.setElement(this.template())
-		radio.on('unrender:LoginView',this.unrender, this)
+    // Create temporary bypass button
+    var button = document.createElement('button')
+    this.$('.ofcourse-vcenter2').append(button)
+    this.$('button').on('click',function() {
+      workspace.navigate('usr/home',{trigger:true})
+    })
+    // end button
+    radio.on('unrender:LoginView',this.unrender, this)
     radio.on('render:LoginView',this.render,this)
     radio.on('unrender',this.unrender,this)
-	},
+  },
   template: JADE.login,
-	render: function(location) {
+  render: function(location) {
     var location = location || this.defaultLocation
     $(location).append(this.$el)
   },
-	unrender: function(){
+  unrender: function(){
     this.$el.remove()
-	}
+  }
 });
