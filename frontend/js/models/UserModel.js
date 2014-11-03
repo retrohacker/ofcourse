a4c5f3d628c282e9a66d0c09310ec4aaf08557ee
root <root@localhost.localdomain>
2014-11-03 16:39:43 -0600

    updated UserModel.js model added model code to database backend pair programming ftw

--- frontend/js/models/UserModel.js
+++ frontend/js/models/UserModel.js
@@ -1,10 +1,23 @@
 //User model
 //This is data that will be stored in the database
-var User = Backbone.Model.extend({
+var UserModel = Backbone.Model.extend({
 	defualts:{
+		id: -1,
 		firstName: 'First Name',
 		lastName: 'Last Name',
 		university: 'University',
 		email: 'Email Address'
+	},
+	validate:function(attributes,options){
+		console.log("VALIDATING")
+		if(attributes.id < 0){
+			return 'id error'
+		}
+		if(attributes.email){
+			var re = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+			if(typeof attributes.email != 'string' || !attributes.email.match(re))
+				return 'email not valid error'
+		}
 	}
 });
+
