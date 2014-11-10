5c3780241a0f6561c110e1886e8cf8e2cf338c09
William Blankenship <william.jblankenship@gmail.com>
2014-11-10 16:49:07 -0600

    It would appear that I mucked things up. zapp now works with uniview.

--- frontend/js/zapp.js
+++ frontend/js/zapp.js
@@ -45,7 +45,30 @@ var Workspace = Backbone.Router.extend({
   'uniSelect': function(){
     var sidebarState = false; //hidden
     radio.trigger('unrender')
-    this.loginView = new LoginView({radio: radio}).render();
+    var sidebar = new SidebarView({radio:radio})
+      .render()
+    var taskbar = new TaskbarView({radio: radio})
+      .addButtonLeft(new TaskbarButtonView({
+        className:'fa fa-fw fa-bars',
+        onClick: function () {
+          if(!sidebarState) {
+            taskbar.$el.css('transform','translateX(25%)')
+            sidebar.$el.css('transform','translateX(0)')
+          } else {
+            taskbar.$el.css('transform','translateX(0)')
+            sidebar.$el.css('transform','translateX(-100%)')
+          }
+          sidebarState = !sidebarState
+        }
+      }))
+      .addButtonRight(new TaskbarButtonView({
+        className:'fa fa-fw fa-paper-plane-o',
+        onClick: function() {
+          console.log("Add Event!")
+        }
+      }))
+      .render()
+    this.uniSelectView = new UniSelectView({radio: radio, universities: universityCollection.toJSON(), user: this.user}).render()
   },
   'calendar': function(){
     var sidebarState = false; //hidden
