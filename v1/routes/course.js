1dec482b41dd545585e2de9dc071f6cf3f75933c
Samuel Coe <sam@localhost.localdomain>
2014-11-22 06:09:48 -0600

    Added later.js and event replication

--- v1/routes/course.js
+++ v1/routes/course.js
@@ -5,6 +5,7 @@ var session = require('../db/session.js')
 var passport = require('passport')
 var User = require('../db/User.js')
 var CourseModel = require('../models/CourseModel.js')
+var later = require('later')
 
 router.use(bodyParser.json())
 router.use(session)
@@ -17,6 +18,22 @@ router.post('/course',function(req,res) {
   var course = new CourseModel()
   if(!course.set(req.body,{validate:true}))
     return res.status(400).json({e:user.validationError})
+  //Create children events!
+  var events = JSON.parse(req.body.events)
+  for (item in events){
+    var sched = later.parse.cron(events[item].cron)
+    var dates = later.schedule(sched).next(1092,course.attributes.start,course.attributes.end)
+    for (events in dates){
+      var courseEvent = new EventModel({
+        parentid: course.attributes.id,
+        courseid: course.attributes.id,
+        title: course.attributes.title,
+        start: dates[events].toJSON(),
+        end: dates[events].setSeconds(dates[events].getSeconds() + events[item].dur).toISOString(),
+        type: 0
+      });
+    }
+  }
   User.getUniversityByUserID(req.user.profile.id,function(e,university) {
     if(e) return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
     if(!university) return res.status(400).json({e:user.validationError})
