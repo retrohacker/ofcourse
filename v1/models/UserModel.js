298685d49053a29b82b6e61c779bd8fbea71c3c9
Brian Gunn <runningoutofcreativenames@gmail.com>
2014-11-21 19:36:28 -0600

    removed backend UserModel check if university is a string, we store university as a number now (university_id)

--- v1/models/UserModel.js
+++ v1/models/UserModel.js
@@ -11,8 +11,8 @@ var UserModel = module.exports = Backbone.Model.extend({
       return 'expected string for firstName'
 	  if(typeof attributes.lastName != 'string')
       return 'expected string for lastName'
-	  if(attributes.university && typeof attributes.university != 'string')
-      return 'expected string for university'
+	  //if(attributes.university && typeof attributes.university != 'string')
+      //return 'expected string for university'
     if(typeof attributes.email != 'string')
       return 'expected string for email'
     if(attributes.email){
