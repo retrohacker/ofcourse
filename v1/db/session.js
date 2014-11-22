dd04cb8db4e20ff8d548aba82999f143268fc45a
William Blankenship <william.jblankenship@gmail.com>
2014-11-21 18:54:01 -0600

    Authenticate to redis and handle errors

--- v1/db/session.js
+++ v1/db/session.js
@@ -4,6 +4,15 @@ var RedisStore = require('connect-redis')(session)
 var redis = require('redis')
 
 var client = (isDev) ? redis.createClient() : redis.createClient('10982,pub-redis-10982.us-east-1-2.5.ec2.garantiadata.com')
+client.on('connect',function() {
+  client.auth('4mgrkMOXRqu9TbkE',function() {
+    console.log("Connected to Redis")
+  })
+})
+
+client.on('error',function(e) {
+  console.log("Redis Error: "+e)
+})
 
 module.exports = session({
   secret: '#fuckitshipit',
