3ed7d834af5024cb12eb9f6e0aa8331ded9463f1
William Blankenship <william.jblankenship@gmail.com>
2014-11-28 19:05:40 -0600

    Removed redis password from repo

--- v1/db/session.js
+++ v1/db/session.js
@@ -5,7 +5,7 @@ var redis = require('redis')
 
 var client = (process.env.REDIS_URL) ? redis.createClient(process.env.REDIS_PORT,process.env.REDIS_URL) : redis.createClient()
 
-client.auth('4mgrkMOXRqu9TbkE',function() {
+client.auth(process.env.REDIS_AUTH,function() {
   console.log("Connected to Redis")
 })
 client.on('error',function(e) {
