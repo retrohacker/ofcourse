33f09b315abbda5dd541e155ded1c586834a4a1f
Sam Coe <samuel.m.coe@gmail.com>
2014-12-07 11:13:48 -0600

    Added support for OSX dev environment using boot2docker setup

--- v1/db/database.js
+++ v1/db/database.js
@@ -2,9 +2,14 @@ var pg = module.exports = require('pg-query')
 var isDev = require('isdev')
 var connString
 var init = require('./init.js')
+var platform = process.platform
 
-if(isDev)
-  connString = "postgres://postgres@127.0.0.1:5432/postgres"
+if(isDev){
+  if(platform == "darwin")
+    connString = "postgres://postgres@192.168.59.103:5432/postgres"
+  else
+    connString = "postgres://postgres@127.0.0.1:5432/postgres"
+}
 else
   connString = "postgres://gfsfillahxrvsa:40mI24y9S5M66bOYQxGWz5og4w@ec2-54-197-227-238.compute-1.amazonaws.com:5432/d30k7bgpai6hjm"
 
