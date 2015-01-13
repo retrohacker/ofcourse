e8194ba2ad3725ac4e2d13befb51a1229fa57ef7
Sam Coe <samuel.m.coe@gmail.com>
2015-01-12 20:00:06 -0600

    Am I doing this right? Closer to working...

--- v1/routes/course.js
+++ v1/routes/course.js
@@ -19,38 +19,41 @@ router.use(passport.session())
 //CourseModel
 router.post('/',function(req,res) {
   if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
-  var course = new CourseModel()
+    var course = new CourseModel()
   if(!course.set(req.body,{validate:true})) {
     return res.status(400).json({e:user.validationError})
   }
 
   async.waterfall([
     function getUserUniversity(cb){
-      User.getUniversityByUserID(req.user.profile.id,function(e,university,cb) {
+      User.getUniversityByUserID(req.user.profile.id,function(e,university) {
         //dont do this, remove this for production build, gives attackers too much info
         if(e) return res.status(500).json(e)
         if(!university) return res.status(400).json(e)
         course.set('university',university)
+        cb(e,course)
       })
     },
     function addUserEvent(course, cb){
-      User.addCourse(course,req.user.profile.id,function(e,id,cb){
+      User.addCourse(course,req.user.profile.id,function(e,id){
         var parent = new ParentEventModel()
         parent.set(course.toJSON())
         parent.set('cid',id)
         cid = id
+        cb(e,cid)
       })
     },
     function addUserParentEvent(cid, cb){
       var events = req.body.events
       var client = new pg.Client(db.connectionParameters)
-      cb()
+      cb(null,client,events)
     },
-    function connect(cb){
-      client.connect(function(e,cb){
+    function connect(client ,events, cb){
+      client.connect(function(e){
+        cb(e,client,events)
       })
     },
-    function query(cb){
+    function query(client, events, cb){
       client.query('BEGIN()', function(e, result){
         for (item in events){
           var sched = later.parse.cron(events[item].cron)
@@ -75,61 +78,15 @@ router.post('/',function(req,res) {
           }
         }
         client.end()
+        cb(e)
       })
     }
   ],
   function(e){
-    if (e) return fail(e)
+    if (e) console.log("Fail" + e)
   })
 })
-/*
-  User.getUniversityByUserID(req.user.profile.id,function(e,university) {
-    if(e) return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
-    if(!university) return res.status(400).json(e)
-    course.set('university',university)
-    User.addCourse(course,req.user.profile.id,function(e,id){
-      if(e) console.log(e)
-      var parent = new ParentEventModel()
-      parent.set(course.toJSON())
-      parent.set('cid',id)
-      cid = id
-      User.addParentEvent(parent, function(e,id){
-        //Create children events!
-        var events = req.body.events
-        //Creat client and transfer out of pool
-        var client = new pg.Client(db.connectionParameters)
-        client.connect(function(e) {
-          client.query('BEGIN()', function(e, result){
-            for (item in events){
-              var sched = later.parse.cron(events[item].cron)
-              var courseStart = new Date(course.attributes.start)
-              var courseEnd = new Date(course.attributes.end)
-              var dates = later.schedule(sched).next(1092,courseStart,courseEnd)
-              for (date in dates){
-                var courseEvent = new EventModel({
-                  userid: req.user.profile.id,
-                  parentid: id,
-                  courseid: cid,
-                  title: course.attributes.title,
-                  start: dates[date].toJSON(),
-                  end: new Date(dates[date].setSeconds(dates[date].getSeconds() + events[item].duration)).toISOString(),
-                  type: 0
-                });
-                var results = db(User.insertCommand(EventModel,courseEvent.toJSON()), function(e, rows, result) {
-                  if(e) console.log(e)
-                  res.write(JSON.stringify({id:result.rows[0].id}))
-                  return res.end()
-                })
-              }
-            }
-            client.end();
-          })
-        })
-      })
-    })
-  })
-})
-*/
+
 //CourseCollection
 router.get('/courses',function(req,res) {
   if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
