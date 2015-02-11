c9e7707a29c1acb2c2a286954798b16670dc7c95
catHash <bgunn@siu.edu>
2015-02-10 18:44:04 -0600

    added logging to backend

--- v1/routes/auth.js
+++ v1/routes/auth.js
@@ -36,8 +36,10 @@ passport.use(new LocalStrategy({
   },
   function(username, password, done) {
     db.user.getUserByEmail(username,function(e,user){
-      if(e) return done(e, false, { message: 'Incorrect username.' });
-      if(user){
+      if(e) {
+	    logger.error('login error', e)  
+	    return done(e, false, { message: 'Incorrect username.' });
+      }if(user){
         return done(null, user.id);
       }
       return done(null, false,  { message: 'authentication error...' })
--- v1/routes/user.js
+++ v1/routes/user.js
@@ -5,6 +5,7 @@ var passport = require('passport')
 
 var db = require('../db')
 var models = require('../models')
+var logger = require('../../logger')
 
 router.use(bodyParser.json())
 router.use(db.session)
@@ -14,7 +15,10 @@ router.use(passport.session())
 router.get('/course/:id/events', function (req, res, next) {
   if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
   db.user.getEventsByCourseID(req.user.profile.id,req.params.id,function(e,events) {
-    if(e) return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
+    if(e) {
+		logger.error('error fetching events', e)
+		return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
+    }
     if(!events) res.status(500).json(new Error('no events found'))
     res.status(200).json(events)
   })
@@ -26,10 +30,15 @@ router.post('/',function(req,res) {
     return res.status(400).json({e:user.validationError})
   }
   db.user.insert(user,function(e,id) {
-    if(e) return res.status(500).json(e)
+    if(e) {
+	  logger.error('error saving user data to database', e)
+	  return res.status(500).json(e)
+    }
     req.login(id,function(e) {
-      if(e) return res.status(500).json(e)
-      return res.status(201).json({id:id})
+      if(e) {
+		logger.error('login error', e)
+		return res.status(500).json(e)
+      }return res.status(201).json({id:id})
     })
   })
 })
@@ -38,11 +47,15 @@ router.put('/',function(req,res) {
   if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Not Logged In"))
    var user = new models.User()
   if(!user.set(req.body,{validate:true})){
+	logger.error('new user registration error', e)  
     return res.status(400).json({e:user.validationError})
   }
   user.set('id',req.user.profile.id)
   db.user.update(user,function(e,id) {
-    if(e) return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
+    if(e) {
+	  logger.error('new user update error', e)  
+	  return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
+    }
     else return res.status(201).json({id:id})
   })
 })
@@ -50,8 +63,10 @@ router.put('/',function(req,res) {
 router.get('/',function(req,res) {
   if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
   db.user.get(req.user.profile.id,function(e,user) {
-    if(e) return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
-    if(!user) res.status(500).json(new Error('user not found'))
+    if(e) {
+	  logger.error('error: could not fetch user', e)  
+	  return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
+    }if(!user) res.status(500).json(new Error('user not found'))
     res.status(200).json(user)
   })
 })
