1847e1d5f527c16930a8d32d8d3f3fc4b50cff03
catHash <bgunn@siu.edu>
2015-02-23 21:49:14 -0600

    fixed all the error returns that actually return {} added logging to all database error handlers in routes

--- v1/routes/university.js
+++ v1/routes/university.js
@@ -10,10 +10,13 @@ router.use(passport.initialize())
 router.use(passport.session())
 
 router.get('/universities',function(req,res) {
-  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
+  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json("Please login")
   db.user.getUniversities(function(e,universities) {
-    if(e) return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
-    if(!universities) res.status(500).json(new Error('user not found'))
-    res.status(200).json(universities)
+    if(e) {
+		logger.error('database error getting university', e)
+		return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
+    }
+    if(!universities) return res.status(500).json('user not found')
+    return res.status(200).json(universities)
   })
 })
--- v1/routes/user.js
+++ v1/routes/user.js
@@ -13,13 +13,13 @@ router.use(passport.initialize())
 router.use(passport.session())
 
 router.get('/course/:id/events', function (req, res, next) {
-  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
+  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json("Please login")
   db.user.getEventsByCourseID(req.user.profile.id,req.params.id,function(e,events) {
     if(e) {
-		logger.error('error fetching events', e)
+		logger.error('database error fetching events', e)
 		return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
     }
-    if(!events) res.status(500).json(new Error('no events found'))
+    if(!events) res.status(500).json('no events found')
     res.status(200).json(events)
   })
 });
@@ -31,12 +31,12 @@ router.post('/',function(req,res) {
   }
   db.user.insert(user,function(e,id) {
     if(e) {
-	  logger.error('error saving user data to database', e)
+	  logger.error('database error saving user data to database', e)
 	  return res.status(500).json(e)
     }
     req.login(id,function(e) {
       if(e) {
-		logger.error('login error', e)
+		logger.error('redis/passport login error', e)
 		return res.status(500).json(e)
       }return res.status(201).json({id:id})
     })
@@ -44,16 +44,16 @@ router.post('/',function(req,res) {
 })
 
 router.put('/',function(req,res) {
-  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Not Logged In"))
+  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json("Not Logged In")
    var user = new models.User()
   if(!user.set(req.body,{validate:true})){
-	logger.error('new user registration error', e)  
+	logger.error('new user registration model validation error', e)  
     return res.status(400).json({e:user.validationError})
   }
   user.set('id',req.user.profile.id)
   db.user.update(user,function(e,id) {
     if(e) {
-	  logger.error('new user update error', e)  
+	  logger.error('database insert new user error', e)  
 	  return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
     }
     else return res.status(201).json({id:id})
@@ -61,12 +61,12 @@ router.put('/',function(req,res) {
 })
 
 router.get('/',function(req,res) {
-  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json(new Error("Please login"))
+  if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json("Please login")
   db.user.get(req.user.profile.id,function(e,user) {
     if(e) {
-	  logger.error('error: could not fetch user', e)  
+	  logger.error('database error: could not fetch user', e)  
 	  return res.status(500).json(e)//dont do this, remove this for production build, gives attackers too much info
-    }if(!user) res.status(500).json(new Error('user not found'))
-    res.status(200).json(user)
+    }if(!user) return res.status(500).json('user not found')
+    return res.status(200).json(user)
   })
 })
