7cd62f72dbac154e7694910b5f8aaded8ed621bd
William Blankenship <william.jblankenship@gmail.com>
2015-03-03 17:30:06 -0600

    Removed uneccesary client

--- v1/routes/user.js
+++ v1/routes/user.js
@@ -60,14 +60,11 @@ router.get('/',function(req,res) {
 router.get('/courses', function (req, res, next) {
   if(!req.user || !req.user.profile || !req.user.profile.id) return res.status(401).json("Please login")
 
-  //
-  var client = new pg.Client(db.db.connectionParameters)
-
   async.waterfall([
     function getUserCourseIDs(cb){
       db.user.getUserCourseIDs(req.user.profile.id, function(e,courseIDs){
 	  if(e) {
-        logger.error('database error: could not fetch user', e)  
+        logger.error('database error: could not fetch user', e)
       }
       return cb(e, courseIDs)
       })
@@ -76,16 +73,15 @@ router.get('/courses', function (req, res, next) {
       async.each(courseIDs,function(course, cb){
 		    db.user.getCourse(course.cid,function(e,course){
 	        if(e) {
-            logger.error('database error: /user/courses: ', e)  
+            logger.error('database error: /user/courses: ', e)
           }
-          res.write(JSON.stringify(course)) 
+          res.write(JSON.stringify(course))
           return res.end()
-        }) 
+        })
       },cb)
     }
   ],
   function(e,courses){
-    client.end()    
     if(e) {
       logger.error('get courses error ', e)
       return res.status(500).end(e.stack+"\n"+JSON.stringify(e))
